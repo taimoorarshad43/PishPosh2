a01f14267b86fca4e36c176371b90494abda05d6
Taimoor Arshad <taimoor.arshad43@gmail.com>
2025-05-09 13:50:51 -0400

    Wrapping App component in BrowserRouter to fix test passing

--- react/App.test.jsx
+++ react/App.test.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import App from "./src/App";
 
 test('renders without crashing', () => {
-  render(<App />);
-//   expect(screen.getByText(/vite/i)).toBeInTheDocument(); // adjust based on your App's content
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  //   expect(screen.getByText(/vite/i)).toBeInTheDocument(); // adjust based on your App's content
 });
