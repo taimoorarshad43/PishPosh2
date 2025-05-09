0f9ce3a460c492e936a936b1435ecc6f1539e772
Taimoor Arshad <taimoor.arshad43@gmail.com>
2025-05-09 16:52:50 -0400

    Adding smoke tests for standard components

--- react/App.test.jsx
+++ react/App.test.jsx
@@ -2,13 +2,53 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
-import App from "./src/App";
+import { test } from "vitest";
 
-test('renders without crashing', () => {
+import App from "./src/App";
+import ProductDetail from "./src/ProductDetail"
+import UserDetail from "./src/UserDetail"
+import Login from "./src/Login"
+import Signup from "./src/Signup"
+import Cart from "./src/Cart"
+
+// Doing a standard battery of smoke tests
+
+test('Main app component renders without crashing', () => {
   render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
   );
+
   //   expect(screen.getByText(/vite/i)).toBeInTheDocument(); // adjust based on your App's content
 });
+
+test('Product Detail renders without crashing', () => {
+    render(<ProductDetail/>);
+})
+
+test('UserDetail renders without crashing', () =>{
+    render(<UserDetail/>);
+})
+
+test('Login renders without crashing', () =>{
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );})
+
+test('Signup renders without crashing', () =>{
+  render(
+    <BrowserRouter>
+      <Signup />
+    </BrowserRouter>
+  );
+})
+
+test('Cart renders without crashing', () =>{
+  render(
+      <Cart />
+  );
+})
+
